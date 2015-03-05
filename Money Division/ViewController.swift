9f36044e8560f793c1ee0798a66b76afc889bb40
andreiarsa <andreiarsa@icloud.com>
2015-03-05 13:07:02 -0300

    adicionei meu nome no copyright

--- Money Division/ViewController.swift
+++ Money Division/ViewController.swift
@@ -3,7 +3,7 @@
 //  Money Division
 //
 //  Created by Daphne Schinazi and Andreia de Sa on 3/5/15.
-//  Copyright (c) 2015 Daphne Schinazi. All rights reserved.
+//  Copyright (c) 2015 Daphne Schinazi and Andreia de Sa. All rights reserved.
 //
 
 import UIKit
